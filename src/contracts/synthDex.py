57a8fda895599b37b84fd287bc35bec8c757cd04
waylad <waylad42@gmail.com>
2022-08-29 08:55:35 +0200

    Add sBTC and sETH to DEX

--- src/contracts/synthDex.py
+++ src/contracts/synthDex.py
@@ -20,21 +20,76 @@ class SynthDex(sp.Contract):
         sp.verify(self.data.administrator == sp.sender, message = self.error.notAdmin())
 
     @sp.entry_point
-    def buySynthUsd(self, amount):
-
+    def buySynthUsd(self):
         # TODO: Get XTZ/USD price from Harbinger https://better-call.dev/ghostnet/KT1ENe4jbDE1QVG1euryp23GsAeWuEwJutQX/storage/big_map/25877/keys
-        
-        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sUSD_contract ,entry_point="mint").open_some()
-        dataToBeSent = sp.record(address = sp.sender, amount = amount)
+        # Assuming 2 USD
+        token_amount = sp.local( 'token_amount', sp.utils.mutez_to_nat(sp.amount) / 2)
+        sp.trace('buySynthUsd token_amount')
+        sp.trace(token_amount.value)
+
+        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sUSD_contract, entry_point="mint").open_some()
+        dataToBeSent = sp.record(address = sp.sender, amount = token_amount.value)
         sp.transfer(dataToBeSent, sp.mutez(0), contractParams)
  
     
     @sp.entry_point
-    def sellSynthUsd(self, amount, withdraw_amount):
-        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sUSD_contract ,entry_point="burn").open_some()
+    def sellSynthUsd(self, amount):
+        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sUSD_contract, entry_point="burn").open_some()
         dataToBeSent = sp.record(address = sp.sender, amount = amount)
         sp.transfer(dataToBeSent, sp.mutez(0), contractParams)
-        # sp.send(sp.sender, withdraw_amount)
+
+        xtz_amount = sp.local( 'xtz_amount', amount * 2)
+        sp.trace('sellSynthUsd xtz_amount')
+        sp.trace(xtz_amount.value)
+        sp.send(sp.sender, sp.utils.nat_to_mutez(xtz_amount.value))
+
+    @sp.entry_point
+    def buySynthEth(self):
+        # TODO: Get XTZ/USD and ETH/USD price from Harbinger https://better-call.dev/ghostnet/KT1ENe4jbDE1QVG1euryp23GsAeWuEwJutQX/storage/big_map/25877/keys
+        # Assuming 1500 USD
+        token_amount = sp.local( 'token_amount', sp.utils.mutez_to_nat(sp.amount) / 1500)
+        sp.trace('buySynthEth token_amount')
+        sp.trace(token_amount.value)
+
+        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sETH_contract, entry_point="mint").open_some()
+        dataToBeSent = sp.record(address = sp.sender, amount = token_amount.value)
+        sp.transfer(dataToBeSent, sp.mutez(0), contractParams)
+ 
+    
+    @sp.entry_point
+    def sellSynthEth(self, amount):
+        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sETH_contract, entry_point="burn").open_some()
+        dataToBeSent = sp.record(address = sp.sender, amount = amount)
+        sp.transfer(dataToBeSent, sp.mutez(0), contractParams)
+
+        xtz_amount = sp.local( 'xtz_amount', amount * 1400)
+        sp.trace('sellSynthEth xtz_amount')
+        sp.trace(xtz_amount.value)
+        sp.send(sp.sender, sp.utils.nat_to_mutez(xtz_amount.value))
+
+    @sp.entry_point
+    def buySynthBtc(self):
+        # TODO: Get XTZ/USD and BTC/USD price from Harbinger https://better-call.dev/ghostnet/KT1ENe4jbDE1QVG1euryp23GsAeWuEwJutQX/storage/big_map/25877/keys
+        # Assuming 20000 USD
+        token_amount = sp.local( 'token_amount', sp.utils.mutez_to_nat(sp.amount) / 20000)
+        sp.trace('buySynthBtc token_amount')
+        sp.trace(token_amount.value)
+
+        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sBTC_contract, entry_point="mint").open_some()
+        dataToBeSent = sp.record(address = sp.sender, amount = token_amount.value)
+        sp.transfer(dataToBeSent, sp.mutez(0), contractParams)
+ 
+    
+    @sp.entry_point
+    def sellSynthBtc(self, amount):
+        contractParams = sp.contract(sp.TRecord(address = sp.TAddress, amount = sp.TNat), self.data.sBTC_contract, entry_point="burn").open_some()
+        dataToBeSent = sp.record(address = sp.sender, amount = amount)
+        sp.transfer(dataToBeSent, sp.mutez(0), contractParams)
+
+        xtz_amount = sp.local( 'xtz_amount', amount * 19000)
+        sp.trace('sellSynthBtc xtz_amount')
+        sp.trace(xtz_amount.value)
+        sp.send(sp.sender, sp.utils.nat_to_mutez(xtz_amount.value))
 
 
 # #############################################################
@@ -58,5 +113,13 @@ def test():
     sc += controller
 
     sc.h1("Buy/Sell sUSD")
-    sc += controller.buySynthUsd(1).run(sender = elon.address, amount = sp.mutez(7000000))
-    sc += controller.sellSynthUsd(amount = 1).run(sender = elon.address)    
+    sc += controller.buySynthUsd().run(sender = elon.address, amount = sp.mutez(7000000))
+    sc += controller.sellSynthUsd(1).run(sender = elon.address)    
+
+    # sc.h1("Buy/Sell sETH")
+    # sc += controller.buySynthEth().run(sender = elon.address, amount = sp.mutez(7000000))
+    # sc += controller.sellSynthEth(1).run(sender = elon.address)    
+
+    # sc.h1("Buy/Sell sBtc")
+    # sc += controller.buySynthBtc().run(sender = elon.address, amount = sp.mutez(7000000))
+    # sc += controller.sellSynthBtc(1).run(sender = elon.address)    
